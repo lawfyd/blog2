8f954345b9a32f74947b8a4166b3f7c18b23bb13
lawfyd <dimonst93333@gmail.com>
2019-02-28 18:01:12 +0000

    add seeds:category, users; add factory: posts

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Автор не известен',
+                'email' => 'author_unknown@gmail.cm',
+                'password' => bcrypt(str_random(16)),
+            ],
+            [
+                'name' => 'Author',
+                'email' => 'author@gmail.cm',
+                'password' => bcrypt('123123'),
+            ]
+        ];
+
+        DB::table('users')->insert($data);
+    }
+}
